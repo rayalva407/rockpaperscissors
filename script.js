11030dbaedd5cae1dbb16c95775e7ab90906eade
rayalva407 <rayalva407@gmail.com>
2024-03-18 17:19:45 -0600

    feat: allows player to play a round

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function getComputerChoice() {
-  choices = ["Rock", "Paper", "Scissors"]
+  choices = ["rock", "paper", "scissors"]
 
   return choices[Math.floor(Math.random()*choices.length)];
+}
+
+function playRound(computerChoice) {
+  playerChoice = prompt("Please enter rock paper or scissors:").toLowerCase();
+  if ((playerChoice === "rock" && computerChoice === "paper") || (playerChoice === "paper" && computerChoice === "scissors") || (playerChoice === "scissors" && computerChoice === "rock")) {
+    return `You Lose! ${computerChoice} beats ${playerChoice}`
+  }
+  else if (computerChoice === playerChoice) {
+    return `Draw! Try Again`
+  }
+  else {
+      return `You Win! ${playerChoice} beats ${computerChoice}`
+  }
 }
