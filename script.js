4f08a5b1306fd164df6d1bfb31a0f5d97ce22071
rayalva407 <rayalva407@gmail.com>
2024-03-18 17:49:51 -0600

    feat: allows the user to play a full game composed of five round

--- script.js
+++ script.js
@@ -7,12 +7,45 @@ function getComputerChoice() {
 function playRound(computerChoice) {
   playerChoice = prompt("Please enter rock paper or scissors:").toLowerCase();
   if ((playerChoice === "rock" && computerChoice === "paper") || (playerChoice === "paper" && computerChoice === "scissors") || (playerChoice === "scissors" && computerChoice === "rock")) {
-    return `You Lose! ${computerChoice} beats ${playerChoice}`
+    return `You Lose! ${computerChoice} beats ${playerChoice}`;
   }
   else if (computerChoice === playerChoice) {
-    return `Draw! Try Again`
+    return `Draw! Try Again`;
   }
   else {
-      return `You Win! ${playerChoice} beats ${computerChoice}`
+    return `You Win! ${playerChoice} beats ${computerChoice}`;
   }
+}
+
+function playGame() {
+  let playerScore = 0;
+  let computerScore = 0;
+
+  for (let i = 1; i < 6; i++) {
+    console.log(`Your Score: ${playerScore}`)
+    console.log(`Computer Score ${computerScore}`)
+    let result = playRound(getComputerChoice());
+    console.log(result)
+
+    if (result.includes("Win")) {
+      playerScore++;
+    }
+    else if (result.includes("Lose")) {
+      computerScore++
+    }
+    else {
+      continue;
+    }
+  }
+
+  if (playerScore > computerScore) {
+    return `You win the game! Computer score: ${computerScore}, Your Score: ${playerScore}`;
+  }
+  else if (playerScore === computerScore) {
+    return `It's a draw! Computer score: ${computerScore}, Your Score: ${playerScore}`
+  }
+  else {
+    return `You lose the game! Computer score: ${computerScore}, Your Score: ${playerScore}`;
+  }
+  
 }
