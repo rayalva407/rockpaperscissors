83646350067f6deb2d58d389a0c8b5b8311b7317
Raymundo Alva <rayalva407@gmail.com>
2024-03-19 00:14:44 -0600

    feat: makes game playable with UI

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ function getComputerChoice() {
 
 function playRound(computerChoice, playerChoice) {
   if ((playerChoice === "rock" && computerChoice === "paper") || (playerChoice === "paper" && computerChoice === "scissors") || (playerChoice === "scissors" && computerChoice === "rock")) {
-    return `You Lose! ${computerChoice} beats ${playerChoice}`;
+    return `You Lose. ${computerChoice} beats ${playerChoice}`;
   }
   else if (computerChoice === playerChoice) {
-    return `Draw! Try Again`;
+    return `Draw!`;
   }
   else {
-    return `You Win! ${playerChoice} beats ${computerChoice}`;
+    return `You Win. ${playerChoice} beats ${computerChoice}`;
   }
 }
 
@@ -50,10 +50,40 @@ function playRound(computerChoice, playerChoice) {
 // }
 
 let buttons = document.querySelectorAll(".btn");
+let cpuScore = 0;
+let playerScore = 0;
+const scoreDisplay = document.querySelector(".score")
+scoreDisplay.innerText = `Score: ${playerScore}`;
 
 
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
-    playRound(getComputerChoice(), button.innerText.toLowerCase())
+    const results = document.createElement("p");
+    results.innerText = playRound(getComputerChoice(), button.innerText.toLowerCase());
+    const div = document.querySelector(".results");
+    div.appendChild(results);
+    
+    if (results.innerText.includes("Win")) {
+       playerScore++;
+       scoreDisplay.innerText = playerScore;
+    }
+    if (results.innerText.includes("Lose")) {
+      cpuScore++
+    }
+
+    if (playerScore === 5) {
+      results.innerText = "Congratulations! You Win the game!"
+      playerScore = 0;
+      cpuScore = 0;
+      scoreDisplay.innerText = playerScore;
+    }
+
+    if (cpuScore === 5) {
+      results.innerText = "Sorry! You lose the game. Try again!"
+      playerScore = 0;
+      cpuScore = 0;
+      scoreDisplay.innerText = playerScore;
+    }
   })
 })
+
