bdbb910a4a1a0e0fe735b270a26b0b98b56eabae
rayalva407 <rayalva407@gmail.com>
2024-03-26 11:16:46 -0600

    chore: code formatting

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 function getComputerChoice() {
-  choices = ["rock ✊", "paper 👋", "scissors ✌️"]
+  choices = ["rock ✊", "paper 👋", "scissors ✌️"];
 
-  return choices[Math.floor(Math.random()*choices.length)];
+  return choices[Math.floor(Math.random() * choices.length)];
 }
 
 function playRound(computerChoice, playerChoice) {
-  if ((playerChoice.includes("rock") && computerChoice.includes("paper") || playerChoice.includes("paper") && computerChoice.includes("scissors") || playerChoice.includes("scissors") && computerChoice.includes("rock"))) {
+  if (
+    (playerChoice.includes("rock") && computerChoice.includes("paper")) ||
+    (playerChoice.includes("paper") && computerChoice.includes("scissors")) ||
+    (playerChoice.includes("scissors") && computerChoice.includes("rock"))
+  ) {
     return `You Lose. ${computerChoice} beats ${playerChoice}`;
-  }
-  else if (computerChoice === playerChoice) {
+  } else if (computerChoice === playerChoice) {
     return `Draw!`;
-  }
-  else {
+  } else {
     return `You Win. ${playerChoice} beats ${computerChoice}`;
   }
 }
@@ -19,51 +21,52 @@ function playRound(computerChoice, playerChoice) {
 let buttons = document.querySelectorAll(".btn");
 let cpuScore = 0;
 let playerScore = 0;
-const scoreDisplay = document.querySelector(".score")
-const reset = document.createElement("button")
-reset.classList.add("reset")
-reset.innerText = "Reset Game"
+const scoreDisplay = document.querySelector(".score");
+const reset = document.createElement("button");
+reset.classList.add("reset");
+reset.innerText = "Reset Game";
 scoreDisplay.innerText = `Score: ${playerScore}`;
 
-reset.addEventListener('click', () => {
-  document.querySelectorAll(".roundResult").forEach(el => el.remove())
-  buttons.forEach(btn => btn.disabled = false)
-})
-
+reset.addEventListener("click", () => {
+  document.querySelectorAll(".roundResult").forEach((el) => el.remove());
+  buttons.forEach((btn) => (btn.disabled = false));
+});
 
 buttons.forEach((button) => {
-  button.addEventListener('click', () => {
+  button.addEventListener("click", () => {
     const results = document.createElement("p");
-    results.classList.add("roundResult")
-    results.innerText = playRound(getComputerChoice(), button.innerText.toLowerCase());
+    results.classList.add("roundResult");
+    results.innerText = playRound(
+      getComputerChoice(),
+      button.innerText.toLowerCase()
+    );
     const div = document.querySelector(".results");
     div.appendChild(results);
-    
+
     if (results.innerText.includes("Win")) {
-       playerScore++;
-       scoreDisplay.innerText = `Score: ${playerScore}`;
+      playerScore++;
+      scoreDisplay.innerText = `Score: ${playerScore}`;
     }
     if (results.innerText.includes("Lose")) {
-      cpuScore++
+      cpuScore++;
     }
 
     if (playerScore === 5) {
       playerScore = 0;
       cpuScore = 0;
       scoreDisplay.innerText = `Score: ${playerScore}`;
-      buttons.forEach(btn => btn.disabled = true)
-      results.innerText = "Congratulations! You Win the game!"
-      results.append(reset)
+      buttons.forEach((btn) => (btn.disabled = true));
+      results.innerText = "Congratulations! You Win the game!";
+      results.append(reset);
     }
 
     if (cpuScore === 5) {
       playerScore = 0;
       cpuScore = 0;
       scoreDisplay.innerText = `Score: ${playerScore}`;
-      buttons.forEach(btn => btn.disabled = true)
-      results.innerText = "Sorry! You lose the game. Try again! "
-      results.append(reset)
+      buttons.forEach((btn) => (btn.disabled = true));
+      results.innerText = "Sorry! You lose the game. Try again! ";
+      results.append(reset);
     }
-  })
-})
-
+  });
+});
